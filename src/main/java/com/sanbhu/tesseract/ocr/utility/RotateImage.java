4e4db188163a428d5bd9093ea00becc66a9d377c
patilbhushanp <patilbhushanp@gmail.com>
2018-07-25 23:51:00 +0530

    Update RotateImage.java

--- src/main/java/com/sanbhu/tesseract/ocr/utility/RotateImage.java
+++ src/main/java/com/sanbhu/tesseract/ocr/utility/RotateImage.java
@@ -1,7 +1,5 @@
 package com.sanbhu.tesseract.ocr.utility;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
@@ -27,57 +25,13 @@ public class RotateImage {
 
 	public File rotateImage(final File inputImageFile, final ImageInformation imageInformation) throws IOException {
 		File outputFile = null;
-		final AffineTransform transform = getExifTransformation(imageInformation);
-		final AffineTransformOp transformOP = new AffineTransformOp(transform, AffineTransformOp.TYPE_BICUBIC);
 		final BufferedImage inputBufferedImage = ImageIO.read(inputImageFile);
-		BufferedImage destinationImage = transformOP.createCompatibleDestImage(inputBufferedImage,
-				(inputBufferedImage.getType() == BufferedImage.TYPE_BYTE_GRAY) ? inputBufferedImage.getColorModel()
-						: null);
-		Graphics2D graphics2d = destinationImage.createGraphics();
-		graphics2d.setBackground(Color.WHITE);
-		graphics2d.clearRect(0, 0, destinationImage.getWidth(), destinationImage.getHeight());
-		destinationImage = transformOP.filter(inputBufferedImage, destinationImage);
+		AffineTransform transform = AffineTransform.getRotateInstance(Math.toRadians(90), inputBufferedImage.getWidth(),
+				inputBufferedImage.getHeight());
+		final AffineTransformOp transformOP = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+		final BufferedImage destinationImage = transformOP.filter(inputBufferedImage, null);
 		outputFile = new File(inputImageFile.getParentFile() + File.separator + "output_rotate.jpg");
 		ImageIO.write(destinationImage, "jpg", outputFile);
 		return outputFile;
-	}
-
-	private AffineTransform getExifTransformation(ImageInformation imageInformation) {
-		final AffineTransform transform = new AffineTransform();
-		switch (imageInformation.orientation) {
-		case 1:
-			break;
-		case 2: // Flip X
-			transform.scale(-1.0, 1.0);
-			transform.translate(-imageInformation.width, 0);
-			break;
-		case 3: // PI rotation
-			transform.translate(imageInformation.width, imageInformation.height);
-			transform.rotate(Math.PI);
-			break;
-		case 4: // Flip Y
-			transform.scale(1.0, -1.0);
-			transform.translate(0, -imageInformation.height);
-			break;
-		case 5: // - PI/2 and Flip X
-			transform.rotate(-Math.PI / 2);
-			transform.scale(-1.0, 1.0);
-			break;
-		case 6: // -PI/2 and -width
-			transform.translate(imageInformation.height, 0);
-			transform.rotate(Math.PI / 2);
-			break;
-		case 7: // PI/2 and Flip
-			transform.scale(-1.0, 1.0);
-			transform.translate(-imageInformation.height, 0);
-			transform.translate(0, imageInformation.width);
-			transform.rotate(3 * Math.PI / 2);
-			break;
-		case 8: // PI / 2
-			transform.translate(0, imageInformation.width);
-			transform.rotate(3 * Math.PI / 2);
-			break;
-		}
-		return transform;
 	}
 }
